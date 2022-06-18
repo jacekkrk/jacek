658c459f2e84477111d45e61ded957b498949638
KubaG <jacek.krk@gmail.com>
2022-06-18 13:58:49 +0200

    do analizy

--- src/main/java/javaStart/inflacjaKrajowaPo2050Roku.java
+++ src/main/java/javaStart/inflacjaKrajowaPo2050Roku.java
@@ -1,7 +1,25 @@
 package javaStart;
 
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.Scanner;
+
 public class inflacjaKrajowaPo2050Roku {
-    public static void main(String[] args) {
-        System.out.print(" first\n secend\n third");
-    }
+        public static boolean isVowel(char ch) {
+                char[] vowels = {'A', 'E', 'I', 'O', 'U'};
+                char character = Character.toUpperCase(ch);
+                for (char x: vowels) {
+                        if (x == character) {
+                                return true;
+                        }
+                }
+                return false;
+        }
+
+        /* Do not change code below */
+        public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                char letter = scanner.nextLine().charAt(0);
+                System.out.println(isVowel(letter) ? "YES" : "NO");
+        }
 }
