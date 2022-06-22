4d30b53251536704e1d8fdaf1955052a4d5abee3
KubaG <jacek.krk@gmail.com>
2022-06-22 12:18:51 +0200

    next arre

---
+++ src/main/java/javaStart/countArrGreterThen.java
@@ -0,0 +1,25 @@
+package javaStart;
+
+import java.util.Scanner;
+
+public class countArrGreterThen {
+    public static void main(String[] args) {
+        // put your code here
+        final var scanner = new Scanner(System.in);
+        final var n = scanner.nextInt();
+        final var arr = scanner.tokens().mapToInt(Integer::parseInt).limit(n).toArray();
+        final var numOccurring = scanner.nextInt();
+        var count = 0;
+
+        for (int i : arr) {
+            if (i > numOccurring) {
+                count+= i;
+            }
+
+        }
+
+      System.out.println(count);
+
+
+    }
+}
